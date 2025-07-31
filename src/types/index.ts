ba2058d0701ba3c7d10bc555251d145691db7cb1
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2025-07-31 18:23:10 +0300

    Refactor: update ILevel interface to use GameDifficulty type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,19 @@
 export interface ILevel {
   label: string;
-  value: string;
+  value: GameDifficulty;
   matrix: [number, number];
+  pairs: number;
+}
+
+export interface GridItem {
+  id: string;
+  imageId: string;
+  isFlipped: boolean;
+  isMatched: boolean;
+}
+
+export enum GameDifficulty {
+  Easy = "easy",
+  Medium = "medium",
+  Hard = "hard",
 }
